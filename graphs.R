7c8a9ee453168d8f6a8d92e842d5209d2527337d
Lisong G <lisong.guo@icloud.com>
2016-01-20 22:38:45 +0100

    legend and line type

--- graphs.R
+++ graphs.R
@@ -7,15 +7,14 @@ entropy <- - x * log(x) - (1-x) * log(1-x)
 # The gini impurity function for a two-class classification.
 gini_impurity <- 1 - x ^ 2 - (1-x) ^ 2
 
-# Create box around plot
-box()
 
 # Plot the distribution of entropy first.
-plot(x, entropy, type="l", col="blue", 
-	 xlab = "probability", ylab="Gini impurity / Entropy")
+plot(x, entropy, type="l", col="red", 
+	 xlab = "probability", ylab="Gini impurity / Entropy", lty=1)
 
 # Plot the gini_impurity further.
-lines(x, gini_impurity, col="red")
+lines(x, gini_impurity, col="blue", lty=20)
 
-
+legend(0.01, 0.7, c("Entropy", "Gini Impurity"),
+	col=c("red", "blue"), lty=c(1, 20))
 
