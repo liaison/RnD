07ed34fdf8c13f5765e81646a7fd049a07a37ab8
Lisong Guo <lisong.guo@amadeus.com>
2016-03-22 17:20:09 +0100

    parse text into a data frame

--- scripts.R
+++ scripts.R
@@ -84,6 +84,20 @@ elem.title <-mylist[["title"]]
 #             Data source 
 ###########################################
 
+mydata <- data.frame(age=numeric(0), gender=character(0), weight=numeric(0))
+
+# mydata <- edit(mydata)
+# fix(mydata)
+
+mydata.txt <- "
+  age gender weight
+  25 m 166
+  30 f 115
+  18 f 120
+"
+
+mydata <- read.table(header=TRUE, text=mydata.txt)
+
 
 
 
