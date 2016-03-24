20c105b622a0bf50b2dbe8a42c1a292fc0ff7283
Lisong G <lisong.guo@icloud.com>
2016-03-24 22:09:47 +0100

    plot lm

--- graphs.R
+++ graphs.R
@@ -1,4 +1,10 @@
 
+
+############# 
+# Compare the distribution of entropy and gini impurity functions which
+#   are used in the decision tree algorithm.
+############# 
+
 x <- seq(0, 1, 0.05)
 
 # The entropy function for a two-class classification.
@@ -17,4 +23,24 @@ lines(x, gini_impurity, col="blue", lty=20)
 
 legend(0.32, 0.25, c("Entropy", "Gini Impurity"),
 	col=c("red", "blue"), lty=c(1, 20))
+
+
+################
+# save the grpah into pdf.
+# Other than that, one can save it as png(), jpeg(), bmp() and postscript() etc.
+pdf("mtcars.pdf")
+
+attach(mtcars)
+plot(wt, mpg)
+abline(lm(mpg ~ wt))
+title("Regression of MPG on Weight")
+detach(mtcars)
+dev.off()
+################
+
+
+# create a new canvas to draw the following graph.
+# dev.new()
+
+
 
