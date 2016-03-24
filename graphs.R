8466905d405440a803dbfa5d3a1ddaa60b622258
Lisong G <lisong.guo@icloud.com>
2016-03-24 22:59:58 +0100

    a dotted line graph

--- graphs.R
+++ graphs.R
@@ -42,5 +42,38 @@ dev.off()
 # create a new canvas to draw the following graph.
 # dev.new()
 
+# Retrieve the list of current parameters that can be modified.
+oldPars <- par(no.readonly = TRUE)
+
+dose  <- c(20, 30, 40, 45, 60)
+drugA <- c(16, 20, 27, 40, 60)
+drugB <- c(15, 18, 25, 31, 40)
+
+# pin: the plot dimensions (width, height)
+par(pin=c(2, 3))
+par(lty=2, pch=17)
+
+# plot 'both' the line and the dots.
+plot(dose, drugA, type="b", col="red",
+     lty=2,  # line style
+     lwd=2,  # line width
+     main="Clinical Trials for Drug A", # main title
+     sub="This is hypothetical data",   # subtitle
+     xlab="Dosage",
+     ylab="Drug Response",
+     xlim=c(20, 60),
+     ylim=c(0, 70)
+     )
+
+# reset the parameters back 
+par(oldPars)
+
+# return the list of colors
+colors()
+
+
+
+
+
 
 
