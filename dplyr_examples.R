3cf346b779545ce78b7b315b729e6dc685e84ed2
Lisong Guo <lisong.guo@amadeus.com>
2016-03-24 17:46:28 +0100

    more examples about dplyr

--- dplyr_examples.R
+++ dplyr_examples.R
@@ -39,23 +39,25 @@ tailnum <- distinct(select(flights, tailnum))
 
 str(tailnum)
 
+# randomly select 10 elements from the data set.
+sample_n(flights, 10)
 
 
-############ data ordering
+############ data ordering ############ 
 
 # sort the rows, by default, the asscending order.
 sort_dep_time <- arrange(flights, dep_time)
 
 
 # sort the arr_time row in the descending order.
-arrange(flights, desc(arr_time))
+sort_arr_time <- arrange(flights, desc(arr_time))
 
 
-
-############  data transformation
+############  data transformation  ############ 
 
 # add new columns to the origin ones.
-mutate(flights, gain = arr_delay - dep_delay, speed = distance / air_time * 60)
+new.dataset <- mutate(flights, gain = arr_delay - dep_delay, speed = distance / air_time * 60)
+
 
 
 # keep only the new columns
@@ -66,13 +68,37 @@ transmute(flights, gain = arr_delay - dep_delay, speed = distance / air_time * 6
 
 ############  statistics
 
-
-summarise(, flights, mean(arr_delay))
-
-
+# calculate the mean arrival delay time for each carrier.(remove the NA)
+summarise(group_by(flights, carrier), mean(arr_delay, na.rm = TRUE))
 
 
+#  x %>% f(y)  ==  f(x, y)
+#  The syntax sugar from dplyr that shows the order of operation from left to right.
 
+flights %>%
+  group_by(year, month, day)  %>%
+  select(arr_delay, dep_delay) %>%
+  summarise(
+    arr = mean(arr_delay, na.rm=TRUE),
+    dep = mean(dep_delay, na.rm=TRUE)
+  ) %>%
+  filter(arr > 30 | dep > 30)
+
+
+# The following statement is euivalent to the above one.
+filter(
+  summarise(
+    select(
+      group_by(flights, year, month, day),
+      arr_delay, dep_delay
+    ),
+
+    arr = mean(arr_delay, na.rm=TRUE),
+    dep = mean(dep_delay, na.rm=TRUE)
+  ),
+
+  arr > 30 | dep > 30
+)
 
 
 
