40d7891583d46c7e8a8add39da05626fb91112ea
Lisong G <lisong.guo@icloud.com>
2016-03-29 22:36:08 +0200

    page ranking algorithm in R

--- page_rank.R
+++ page_rank.R
@@ -1,5 +1,82 @@
+library(dplyr)
+
+# set the precision of numeric calculation
+options(digits=7)
+
+input <- read.csv("data/page_rank.csv",
+                  comment.char = "#",
+                  sep=",",
+                  strip.white = TRUE, # strip the white space between separator
+                  stringsAsFactors = TRUE)
 
 
-input <- read.csv("data/page_rank.csv")
+# The result of the page_rank algorithm is actually 
+#   the result of a markov chain process.
+
+# If we represent the graph as the the square matrix,
+#   then the result of page rank is actually 
+#   the eigenvalues * eigenvector of the matrix ? 
+
+
+# Order the matrix by node.
+graph <- input[order(input$Node),]
+graph
+
+# get the set of nodes
+nodes.set <- union(graph$Node, graph$Neighbor)
+nodes.set
+
+
+# damping factor: the possibility that users navigate from one page to another.
+# i.e.the possibility that users stay on the current page is (1-damping.factor)
+damping.factor <- 0.85
+
+
+# set the initial value of the page ranking value as 1.
+# And index the rows with the name of the node.
+page.rank.score <- data.frame(Node = nodes.set,
+                              Score = 1,
+                              row.names = nodes.set)
+
+# count the number of neighbors for each node.
+neighbor.count <-
+  summarise(group_by(graph, Node), Neighbors = length(Neighbor))
+
+# Merge the above fields
+page.rank <- merge(page.rank.score, neighbor.count, all.x = TRUE)
+row.names(page.rank) <- nodes.set
+page.rank
+
+
+while(TRUE) {
+
+  contrib.score <- rep(0, times=length(nodes.set))
+  # index the element with the node names
+  names(contrib.score) <- nodes.set
+
+  # calculate the contributions of each node to its neighbors.
+  for(i in 1:nrow(graph)) {
+    node <- graph[i, "Node"]
+    neighbor <- graph[i, "Neighbor"]
+
+    contrib <-
+      page.rank[node, "Score"] * damping.factor / page.rank[node, "Neighbors"]
+    
+    contrib.score[neighbor] <- contrib.score[neighbor] + contrib 
+  }
+
+  init.score <- (1-damping.factor) / length(nodes.set)
+
+  new.score <- init.score + contrib.score 
+
+  print(page.rank)
+
+  if (all(page.rank$Score == new.score)) {
+    break
+  } else {
+    page.rank$Score <- new.score
+  }
+}
+
 
 
