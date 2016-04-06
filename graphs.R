cb2e641779ffe6b74ac2bca83f4d10d6e5cb3652
Lisong G <lisong.guo@icloud.com>
2016-04-06 23:49:27 +0200

    histograms

--- graphs.R
+++ graphs.R
@@ -229,4 +229,51 @@ fan.plot(slices, labels = labels.country, main = "Fan Plot")
 
 
 
+################
+##  histogram ##
+################
+
+mtcars$mpg
+
+par(mfrow=c(2,2))
+# simple histogram
+hist(mtcars$mpg)
+
+# histogram with specified bins and color
+hist(mtcars$mpg,
+     breaks=12,
+     col="red",
+     xlab="Miles Per Gallon",
+     main="Colored histogram with 12 bins")
+
+
+# histogram with a rug plot
+hist(mtcars$mpg,
+     freq=FALSE,
+     break=12,
+     col="red",
+     xlab="Miles Per Gallon",
+     main="Histogram, rug plot, density curve")
+rug(jitter(mtcars$mpg))
+lines(density(mtcars$mpg), col="blue", lwd=2)
+
+
+# with a normal curve and frame
+x <- mtcars$mpg
+h <- hist(x,
+          breaks=12,
+          col="red",
+          xlab="Miles Per Gallon",
+          main="Histogram with normal curve and box")
+
+xfit <- seq(min(x), max(x), length=40)
+yfit <- dnorm(xfit, mean=mean(x), sd=sd(x))
+yfit <- yfit * diff(h$mids[1:2]) * length(x)
+lines(xfit, yfit, col="blue", lwd=2)
+box()
+
+
+
+
+
 
