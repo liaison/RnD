1813288f0e7d3af9440f103b12a5687abdcb62e2
Lisong G <lisong.guo@icloud.com>
2016-04-10 21:05:21 +0200

    more simple regression and its plot

--- regression.R
+++ regression.R
@@ -16,6 +16,10 @@
 # Simple linear regression, polynomial regression and multiple linear regression
 
 
+
+##############################
+# simple regression
+
 fit <- lm(weight ~ height, data=women)
 
 summary(fit)
@@ -36,8 +40,12 @@ plot(women$height, women$weight,
 abline(fit)
 
 # I() function wraps/escapes the arithmetical operators.
+# Add a quadratic (power 2) to the whole polynomial term.
 fit2 <- lm(weight ~ height + I(height ^ 2), data = women)
 
+# The significance of the squared term (t=13.89) suggests that the inclusion of
+#  the quadratic term improves the model fit.
+# Indeed, as it shows in the following graph, the fitting line is more smooth.
 summary(fit2)
 
 
@@ -46,6 +54,20 @@ plot(women$height, women$weight,
      ylab="Weight (in pounds)")
 
 lines(women$height, fitted(fit2))
+
+
+##############################
+
+library(car)
+
+# the loess fit is rendered as dashed line (lty=2)
+# the pch=19 option displays points as filled circles (default: open circle)
+scatterplot(weight ~ height, data=women,
+            spread=FALSE, smoother.args=list(lty=2), pch=19,
+            main="Women Age 30 - 39",
+            xlab="Height (inches)",
+            ylab="Weight (lbs.)")
+
 
 
 
