7975d493bfeb10f0f54350db9ba7876904f8db6b
Lisong G <lisong.guo@icloud.com>
2016-04-10 19:17:05 +0200

    regression models

---
+++ regression.R
@@ -0,0 +1,19 @@
+
+#######################
+#  Regression Models
+#######################
+
+# methodologies
+
+#  predict "response" (dependent) variable from one or more "predictor"
+#    (independent/explanatory) variables.
+
+# OLS (ordinary least squares) regression, the regression models that
+#    minimize the sum of square residuals
+#   (difference between predicted value and actual value)
+
+# e.g:
+# Simple linear regression, polynomial regression and multiple linear regression
+
+
+
