da3e11d6f7ad34f1a09db1e8be6ba45372b5ba41
Lisong G <lisong.guo@icloud.com>
2016-04-10 19:41:09 +0200

    simple linear regression

--- regression.R
+++ regression.R
@@ -16,4 +16,43 @@
 # Simple linear regression, polynomial regression and multiple linear regression
 
 
+fit <- lm(weight ~ height, data=women)
+
+summary(fit)
+
+women$weight
+
+# get predicted value for all the samples involved in the model.
+fitted(fit)
+
+# get the residual values
+residuals(fit)
+
+
+plot(women$height, women$weight,
+     xlab="Height (in inches)",
+     ylab="Weight (in pounds)")
+
+abline(fit)
+
+# I() function wraps/escapes the arithmetical operators.
+fit2 <- lm(weight ~ height + I(height ^ 2), data = women)
+
+summary(fit2)
+
+
+plot(women$height, women$weight,
+     xlab="Height (in inches)",
+     ylab="Weight (in pounds)")
+
+lines(women$height, fitted(fit2))
+
+
+
+
+
+
+
+
+
 
