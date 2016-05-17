ed3fb99c5d0a49a940358c2ce565fd976e4ae7c3
Lisong G <lisong.guo@icloud.com>
2016-05-17 21:27:17 +0200

    LICENSE

--- regression.R
+++ regression.R
@@ -1,3 +1,19 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 #######################
 #  Regression Models
@@ -70,8 +86,25 @@ scatterplot(weight ~ height, data=women,
 
 
 
+##############################
+# multiple linear regression:  regression invovles multiple variables
+str(state.x77)
+head(state.x77)
 
+states <- as.data.frame(state.x77[, c("Murder", "Population",
+                                      "Illiteracy", "Income", "Frost")])
 
+# the bivariate correlations
+cor(states)
+
+library(car)
+scatterplotMatrix(states, spread=FALSE, smoother.args = list(lty=2),
+                  main="Scatter Plot Matrix")
+
+# from the correlation table and the scatter plot, we could tell:
+# 1). Murder rates rise with population and illiteracy.
+# 2). Murder rates fall with higher income levels and frost
+# 3). Colder states have lower illiteracy rates and population and higher income.
 
 
 
